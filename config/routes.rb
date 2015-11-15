3a93ec7fc2042c559a54dac3d75e32819a1f4abb
Ann Catton <anncatton@gmail.com>
2015-11-15 16:31:55 +0000

    created static routes for each post

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ LighthouseForum::Application.routes.draw do
 
   get 'posts', to: 'posts#index'
 
+  get 'posts/0', to: 'posts#post0'
+  get 'posts/1', to: 'posts#post1'
+  get 'posts/2', to: 'posts#post2'
+  get 'posts/3', to: 'posts#post3'
+
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -56,4 +62,3 @@ LighthouseForum::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
